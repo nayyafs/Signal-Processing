02dd817e368cc33807c290616ec170acc9d2fa95
nayyafs <110149528+nayyafs@users.noreply.github.com>
2023-12-29 17:02:01 +0700

    Add files via upload

---
+++ Teori 3/plot_butterworth_3d.m
@@ -0,0 +1,48 @@
+% Filter Butterworth
+% All frequency values are in Hz.
+Fs = 1;  % Sampling Frequency
+
+N   = 8;      % Order
+Fc1 = 0.104;  % First Cutoff Frequency
+Fc2 = 0.106;  % Second Cutoff Frequency
+
+% Construct an FDESIGN object and call its BUTTER method.
+h  = fdesign.bandpass('N,F3dB1,F3dB2', N, Fc1, Fc2, Fs);
+Hd = design(h, 'butter');
+
+%implementasi filter
+n = 1:1000;
+x = 0.5 + cos(2*pi*n/10) + 0.1*cos(2*pi*1.05*n/10)+0.001*cos(2*pi*1.11*n/10); 
+x2 = 0.1*cos(2*pi*1.05*n/10);
+filtered_signal = filter(Hd, x);
+
+figure;
+subplot(2, 1, 1);
+plot(n, filtered_signal);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Sinyal Output Hasil Filter IIR Sederhana');
+subplot(2, 1, 2);
+plot(n, x2);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Sinyal x2(n) sebagai Referensi');
+
+%periksa hasil output dengan fft
+N = length(filtered_signal);
+
+fft_signal = fft(filtered_signal);
+
+P = abs(fft_signal)/N;
+
+P = P(1:N/2+1);
+P(2:end-1) = 2*P(2:end-1);
+
+freq = (0:(N/2))*Fs/N;
+
+figure;
+plot(freq, P);
+xlabel('Frekuensi (Hz)');
+ylabel('Magnitude');
+title('FFT Sinyal Output Hasil Filter');
+
