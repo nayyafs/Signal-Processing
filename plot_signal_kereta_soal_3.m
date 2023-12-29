419ba4c15beddc1f7dca8344bdff4af836f12dbf
nayyafs <110149528+nayyafs@users.noreply.github.com>
2023-12-29 17:14:07 +0700

    Delete plot_signal_kereta_soal_3.m

--- plot_signal_kereta_soal_3.m
+++
@@ -1,9 +0,0 @@
-[x, Fs] = audioread('kereta.aac');
-maxt = (length(x)-1)/Fs;
-t = [0:1/Fs:maxt];
-
-figure;
-plot(t, x, 'b');
-xlabel('Time(s)');
-ylabel('Amplitude');
-title('Sinyal Audio Palang Pintu Kereta')
