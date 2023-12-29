b2656bf798c4203a550e1bd13a0f17993e0ea942
nayyafs <110149528+nayyafs@users.noreply.github.com>
2023-12-29 17:02:19 +0700

    Add files via upload

---
+++ Teori 3/plot_chebysev_3e.m
@@ -0,0 +1,49 @@
+% Filter Chebysev Type 1
+
+% All frequency values are in Hz.
+Fs = 1;  % Sampling Frequency
+
+N      = 8;      % Order
+Fpass1 = 0.104;  % First Passband Frequency
+Fpass2 = 0.106;  % Second Passband Frequency
+Apass  = 1;      % Passband Ripple (dB)
+
+% Construct an FDESIGN object and call its CHEBY1 method.
+h  = fdesign.bandpass('N,Fp1,Fp2,Ap', N, Fpass1, Fpass2, Apass, Fs);
+Hd = design(h, 'cheby1');
+
+%implementasi filter
+n = 1:1000;
+x = 0.5 + cos(2*pi*n/10) + 0.1*cos(2*pi*1.05*n/10)+0.001*cos(2*pi*1.11*n/10); 
+x2 = 0.1*cos(2*pi*1.05*n/10);
+filtered_signal = filter(Hd, x);
+
+figure;
+subplot(2, 1, 1);
+plot(n, filtered_signal);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Sinyal Output Hasil Filter IIR Sederhana');
+subplot(2, 1, 2);
+plot(n, x2);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Sinyal x2(n) sebagai Referensi');
+
+%periksa hasil output dengan fft
+N = length(filtered_signal);
+
+fft_signal = fft(filtered_signal);
+
+P = abs(fft_signal)/N;
+
+P = P(1:N/2+1);
+P(2:end-1) = 2*P(2:end-1);
+
+freq = (0:(N/2))*Fs/N;
+
+figure;
+plot(freq, P);
+xlabel('Frekuensi (Hz)');
+ylabel('Magnitude');
+title('FFT Sinyal Output Hasil Filter');
